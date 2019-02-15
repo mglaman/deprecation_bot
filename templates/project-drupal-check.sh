286c5030c37779555e6bcf2f954eb230c9b2ce44
Matt Glaman <nmd.matt@gmail.com>
2019-02-14 20:09:14 -0600

    Fix scripts

--- templates/project-drupal-check.sh
+++ templates/project-drupal-check.sh
@@ -3,13 +3,10 @@ curl -O https://ftp.drupal.org/files/projects/drupal-8.7.x-dev.tar.gz
 tar -xzf drupal-8.7.x-dev.tar.gz
 cd drupal-8.7.x-dev
 
-composer config minimum-stability false
-composer config prefer-stable false
-
 if [ -n "$ProjectName" ]; then
-	composer config minimum-stability false
+	composer config minimum-stability dev
 	composer config prefer-stable false
     composer require drupal/$ProjectName
 fi
 
-drupal-check -d $CheckPath --format junit > report.xml
+drupal-check -d $CheckPath --format junit > ../report.xml
